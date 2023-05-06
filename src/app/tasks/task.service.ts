551f323691d54ca242a56cefb8b584b49f79ce8a
Mike Costarella <mcosti@sprynet.com>
2023-05-06 12:30:16 -0400

    Hooked up to Mockoon Api without date filtering.

--- src/app/tasks/task.service.ts
+++ src/app/tasks/task.service.ts
@@ -1,34 +1,41 @@
 import { Injectable } from '@angular/core';
 import { TaskItem } from './task-item.dto';
 import { NewTask } from './new-task.dto';
-import { Observable, BehaviorSubject } from 'rxjs';
+import { Observable, BehaviorSubject, tap, map } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+
+const resourceURL = 'http://localhost:3001/tasks';
 
 @Injectable()
 export class TaskService {
 
   constructor(private httpClient: HttpClient) { }
 
-  private tasks = new BehaviorSubject([
-    new TaskItem("Visit Ann"),
-    new TaskItem("Call Dad"),
-    new TaskItem("Go to gym"),
-    new TaskItem("Wash the dishes"),
-    new TaskItem("Shop for the party")
-  ])
+  private tasks = new BehaviorSubject<TaskItem[]>([])
 
   getAllTasks(): Observable<TaskItem[]> {
-    return this.tasks;
+    this.httpClient.get<TaskItem[]>(resourceURL)
+    //.pipe(tap(console.log))
+    .pipe(map(TaskService.mapTaskItems))
+    //.pipe(tap(console.log))
+    .subscribe(t => this.tasks.next(t))
+    return this.tasks
+  }
+
+  private static mapTaskItems(items: {title: string}[]) {
+    return items.map(item => new TaskItem(item.title))
   }
 
   addTask(newTask: NewTask)
   {
       var updatedTasks = this.tasks.value.concat(new TaskItem(newTask.title));
-      this.tasks.next(updatedTasks);
+      this.httpClient.post(resourceURL, newTask)
+        .subscribe(() => this.tasks.next(updatedTasks));
   }
 
   removeTask(existingTask: TaskItem){
     var updatedTasks = this.tasks.value.filter(task => task != existingTask);
-    this.tasks.next(updatedTasks);
-  }
+    this.httpClient.delete(`${resourceURL}/${existingTask.title}`)
+      .subscribe(() => this.tasks.next(updatedTasks));
+   }
 }
